8bdf9aca2a2206a5dee4744d84c9e05ccd0926b5
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2025-03-02 13:57:36 +0300

    Initial
    API + desktop

---
+++ FileManager/FileManagerClient/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FileManagerClient.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ FileManagerClient/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FileManagerClient.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
